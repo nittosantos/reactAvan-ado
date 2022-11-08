0a95283c43cc5d30cd115a508f1607af46da3eee
Nitto <nitto_kun@hotmail.com>
2022-11-07 23:01:08 -0300

    config rtl and first test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testEnviroment: 'jsdom',
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/stup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
